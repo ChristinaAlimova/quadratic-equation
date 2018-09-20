389632b4ed25c4b18119e05eb2610c7cb61d6a80
Christina Alimova <alimova.christina@gmail.com>
2018-09-20 20:23:39 +0300

    4-th task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var sign = 0;
+  var param;
+  var arr = [];
+  var a;
+  var b;
+  var c;
+  var d;
+  var x1;
+  var x2;
+  var result = [];
+  var num = equation.split(' ');
+
+  for (var i = 0; i < num.length; i++) {
+    if (num[i] == '+') {
+      sign = 1; 
+    }
+    if (num[i] == '-') {
+      sign = -1;
+    }
+
+    if (!isNaN(num[i])) {
+      if (sign !== 0) {
+        param = sign * num[i];
+        arr.push(param);
+      }
+      else {
+        arr.push(parseInt(num[i]));
+      }
+    }
+  }
+
+  a = arr[0];
+  b = arr[1];
+  c = arr[2];
+
+  d = b * b - 4 * a * c;
+  x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+  x2 = (-1 * b - Math.sqrt(d)) / (2 * a);
+  
+
+  result.push(parseInt(x1.toFixed()));
+  result.push(parseInt(x2.toFixed()));
+  result.sort(function(a,b){return a - b});
+  return result;
 }
